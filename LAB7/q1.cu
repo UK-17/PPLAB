f388786015972ab9b0ba57e1ac604916bbdd110b
Utkarsh Kushagra <40884732+UK-17@users.noreply.github.com>
2020-02-20 10:05:26 +0530

    Create q1.cu

---
+++ LAB7/q1.cu
@@ -0,0 +1,80 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+
+__global__ void add (int *A, int *B, int *C) {
+	int idx = blockIdx.x*blockDim.x+threadIdx.x;
+    printf("idx = %d\n", idx);
+    C[idx] = A[idx] + B[idx];
+}
+
+int main () {
+    
+    int M,N;
+    printf("Blocks M:");
+    scanf("%d",&M);
+    printf("Threads N:");
+    scanf("%d",&N);
+
+    
+    // Host copies of the variables
+    int A[M], B[M], C[M];
+
+    int i, j;
+    printf("Enter %d elements in A:\n",M*N);
+    for (i = 0; i < M*N; ++i) 
+        scanf("%d",&A[i]);
+    printf("Enter %d elements in B:\n",M*N);    
+    for (i = 0; i < M*N; ++i) 
+        scanf("%d",&B[i]);
+    
+
+    // Device copies of the variables
+    int *d_a, *d_b, *d_c;
+
+    int size = sizeof(int) * M;
+
+    // Allocate memories to device copies of the objects
+    cudaMalloc((void**)&d_a, size);
+    cudaMalloc((void**)&d_b, size);
+    cudaMalloc((void**)&d_c, size);
+
+    // Copy inputs to device
+    cudaMemcpy(d_a, &A, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, &B, size, cudaMemcpyHostToDevice);
+
+    // Launch kernel onto the device
+    add<<<M, 1>>>(d_a, d_b, d_c);
+
+    // Copy the result back to the host
+    cudaMemcpy(&C, d_c, size, cudaMemcpyDeviceToHost);
+
+    // Output
+    printf("A:\n");
+    for (j = 0; j < M; ++j) {
+        printf("%d\t", A[j]);
+    }
+    printf("\n");
+
+    printf("B:\n");
+    for (j = 0; j < M; ++j) {
+        printf("%d\t", B[j]);
+    }
+    printf("\n");
+
+    printf("A + B:\n");
+    for (j = 0; j < M; ++j) {
+        printf("%d\t", C[j]);
+    }
+    printf("\n");
+
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
+
+	getchar();
+
+    return 0;
+}
