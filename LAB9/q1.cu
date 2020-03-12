a953e75740a6dd2de6b1d65238c89046da35c1e1
Utkarsh Kushagra <40884732+UK-17@users.noreply.github.com>
2020-03-12 10:11:48 +0530

    Create q1.cu

---
+++ LAB9/q1.cu
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+__global__ void kernel(int *a,int *c,int n){
+	int id1=threadIdx.x;
+	int id2=blockIdx.x;
+	int pos=id2*n+id1;
+	c[pos]=1;
+	for(int i=0;i<=id2;i++) c[pos]=a[pos]*c[pos];
+}
+
+int main(){
+	int *a,*b,*c;
+	int *d_a,*d_b,*d_c;
+	printf("Enter the order of the matrix: ");
+	int n;scanf("%d",&n);
+	int size=sizeof(int)*n*n;
+	a=(int *)malloc(size);
+	b=(int *)malloc(size);
+	c=(int *)malloc(size);
+
+	printf("Enter the Matrix A: \n");
+	for(int i=0;i<n*n;i++){
+		scanf("%d",&a[i]);
+	}
+	printf("\n");
+
+	cudaMalloc((void **)&d_a,size);
+	cudaMalloc((void **)&d_b,size);
+	cudaMalloc((void **)&d_c,size);
+
+	cudaMemcpy(d_a,a,size,cudaMemcpyHostToDevice);
+	cudaMemcpy(d_b,b,size,cudaMemcpyHostToDevice);
+
+	kernel<<<n,n>>>(d_a,d_c,n);
+
+	cudaMemcpy(c,d_c,size,cudaMemcpyDeviceToHost);
+	printf("Result Matrix is: \n");
+	for(int i=0;i<n*n;i++){
+		if(i%n==0) printf("\n");
+		printf("%5d ",c[i]);
+	}
+	printf("\n");
+
+	getchar();
+	cudaFree(d_a);
+	cudaFree(d_b);
+	cudaFree(d_c);
+	return 0;
+}
